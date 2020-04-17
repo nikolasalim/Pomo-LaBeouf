f7bd1416ab56db0768283e01fa78b599f691519c
nikolasalim <nikolasalim@gmail.com>
2020-04-17 13:40:48 +0200

    add read-me file

---
+++ README.md
@@ -0,0 +1,46 @@
+# ☞ About
+
+Pomo-LaBeouf is a Shia LaBeouf themed Pomodoro technique website.
+
+#### What is the Pomodoro technique?
+
+It's a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks. Personally, it helps me focus.
+
+#### Why Shia LaBeouf?
+
+I'm a big fan of his "JUST DO IT" ~~inspirational speech~~ meme, and I strongly believe his wise words can help motivate other people.  
+[Watch it here.](https://www.youtube.com/watch?v=ZXsQAXx_ao0&t=1s)
+
+# Contents list
+
+- [Demo](#demo)
+- [Goals](#goals)
+- [Features](#features)
+
+# Demo
+
+To check out the currently deployed version, [click here.](https://nikolasalim.github.io/Pomo-LaBeouf/)
+
+![timer-gif](https://github.com/nikolasalim/Pomo-LaBeouf/tree/master/media/demo/timer.gif)
+![cursor-gif](https://github.com/nikolasalim/Pomo-LaBeouf/tree/master/media/demo/cursor.gif)
+![bg-gif](https://github.com/nikolasalim/Pomo-LaBeouf/tree/master/media/demo/bg.gif)
+
+# Goals
+
+This is my first personal coding project.  
+At that time I was mainly experimenting with making a website more interactive using plain HTML, CSS and Javascript.  
+Breaking it down to the specifics:
+
+- Practice the HTML, CSS and Javascript skills I had learned so far at Codecademy;
+- Expand my knowledge on Git & Github;
+- Play with DOM Objects and different types of media;
+- Experiment with event listeners – specially for the 'following eyes' feature.
+- Explore CSS positioning and displaying, as well as media queries;
+
+# Features
+
+- Functioning timer with start, stop, resume and short/long intervals buttons;
+- Random Shia sound bits for when clicking 'just do it' and stop buttons;
+- Muting feature, in case you're tired of hearing Shia's voice.
+- Several random backgrounds to make the whole experience even weirder;
+- Cursor-following eyes to give it some extra creepyiness.
--- js/main.js
+++ js/main.js
@@ -5,45 +5,46 @@ let remaining;
 
 // timer functionality
 
-function timer(seconds){
-    const now = Date.now();
-    const then = now + seconds * 1000;
-    displayTimeLeft(seconds)
-    clearInterval(countdown)
-    remaining = seconds;   
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  clearInterval(countdown);
+  remaining = seconds;
 
-    countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
 
-        if (secondsLeft < 0){
-            clearInterval(countdown)
-            return;
-        }
-        displayTimeLeft(secondsLeft);
-        
-        remaining = Math.round(secondsLeft)
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
 
-    }, 1000)
-} 
+    remaining = Math.round(secondsLeft);
+  }, 1000);
+}
 
 // timer display
 
-function displayTimeLeft(seconds){
-    const minutes = Math.floor(seconds / 60);
-    const remainderSeconds = Math.floor(seconds % 60);
-    const display = `${minutes > 10 ? minutes : '0' + minutes} : ${remainderSeconds > 9 ? remainderSeconds : '0' + remainderSeconds}`
-    document.getElementById("timer").textContent = display;
-    document.title = display;
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = Math.floor(seconds % 60);
+  const display = `${minutes > 10 ? minutes : "0" + minutes} : ${
+    remainderSeconds > 9 ? remainderSeconds : "0" + remainderSeconds
+  }`;
+  document.getElementById("timer").textContent = display;
+  document.title = display;
 
-    // alarm sound
-    if (display === "00 : 00"){
-        document.title = 'you did it!';
-        if (soundOn){
-            const sound = new Audio();
-            sound.src = "./media/audio/alarm.wav"
-            sound.play();
-        }
+  // alarm sound
+  if (display === "00 : 00") {
+    document.title = "you did it!";
+    if (soundOn) {
+      const sound = new Audio();
+      sound.src = "./media/audio/alarm.wav";
+      sound.play();
     }
+  }
 }
 
 // start, stop & resume buttons
@@ -54,108 +55,135 @@ const longBreakBtn = document.getElementById("900");
 const stopBtn = document.getElementById("stop");
 
 function startTimer() {
-    const seconds = this.id;
-    timer(seconds)
-    isOn = true;
-    hasRunned = true;
-    stopBtn.firstChild.textContent = 'stop';
-};
+  const seconds = this.id;
+  timer(seconds);
+  isOn = true;
+  hasRunned = true;
+  stopBtn.firstChild.textContent = "stop";
+}
 
 function startBreak() {
-    const seconds = this.id;
-    timer(seconds)
-    isOn = true;
-    hasRunned = true;
-    stopBtn.firstChild.textContent = 'stop';
-};
+  const seconds = this.id;
+  timer(seconds);
+  isOn = true;
+  hasRunned = true;
+  stopBtn.firstChild.textContent = "stop";
+}
 
 function stop() {
-    clearInterval(countdown)
-    stopBtn.firstChild.textContent = 'resume';
-    isOn = false;
-};
+  clearInterval(countdown);
+  stopBtn.firstChild.textContent = "resume";
+  isOn = false;
+}
 
 function resume() {
-    stopBtn.firstChild.textContent = 'stop';
-    isOn = true;
-    timer(remaining)
-};
+  stopBtn.firstChild.textContent = "stop";
+  isOn = true;
+  timer(remaining);
+}
 
-startBtn.addEventListener('click', startTimer)
-startBtn.addEventListener('click', function(){
-    // random 'Just do it' sound on click;
-    function randomJustDoItSounds(){
-        const allFiles = ["just-do-it-1.wav", "just-do-it-2.wav", "just-do-it-3.wav", "just-do-it-4.wav", "do-it.wav", "do-it-1.wav", "do-it-2.wav", "dreams-be-dreams.wav", "nothing-is-impossible.wav", "yes-you-can.wav", "make-your-dreams.wav"]
-        const randomNum = Math.floor(Math.random() * allFiles.length)
-        return allFiles[randomNum];
-    };
-    if(soundOn){
-        const justDoItSounds = new Audio();
-        justDoItSounds.src = './media/audio/' + randomJustDoItSounds()
-        justDoItSounds.play();
-    }
+startBtn.addEventListener("click", startTimer);
+startBtn.addEventListener("click", function () {
+  // random 'Just do it' sound on click;
+  function randomJustDoItSounds() {
+    const allFiles = [
+      "just-do-it-1.wav",
+      "just-do-it-2.wav",
+      "just-do-it-3.wav",
+      "just-do-it-4.wav",
+      "do-it.wav",
+      "do-it-1.wav",
+      "do-it-2.wav",
+      "dreams-be-dreams.wav",
+      "nothing-is-impossible.wav",
+      "yes-you-can.wav",
+      "make-your-dreams.wav",
+    ];
+    const randomNum = Math.floor(Math.random() * allFiles.length);
+    return allFiles[randomNum];
+  }
+  if (soundOn) {
+    const justDoItSounds = new Audio();
+    justDoItSounds.src = "./media/audio/" + randomJustDoItSounds();
+    justDoItSounds.play();
+  }
 });
 
-shortBreakBtn.addEventListener('click', startBreak)
+shortBreakBtn.addEventListener("click", startBreak);
 
-longBreakBtn.addEventListener('click', startBreak)
+longBreakBtn.addEventListener("click", startBreak);
 
-stopBtn.addEventListener('click', function() {
+stopBtn.addEventListener("click", function () {
+  if (!hasRunned) {
+    return;
+  }
 
-   if (!hasRunned){
-        return;
-   }
-   
-   if (isOn){
-        stop()
-        // random 'Stop' sound on click;
-        function randomStopSounds(){
-            const allFiles = ["not-gonna-stop-there.wav", "stop-giving-up.wav", "yesterday.wav"]
-            const randomNum = Math.floor(Math.random() * allFiles.length)
-            return allFiles[randomNum];
-        };
-        
-        if(soundOn){
-            const StopSounds = new Audio();
-            StopSounds.src = './media/audio/' + randomStopSounds()
-            StopSounds.play();
-        }
+  if (isOn) {
+    stop();
+    // random 'Stop' sound on click;
+    function randomStopSounds() {
+      const allFiles = [
+        "not-gonna-stop-there.wav",
+        "stop-giving-up.wav",
+        "yesterday.wav",
+      ];
+      const randomNum = Math.floor(Math.random() * allFiles.length);
+      return allFiles[randomNum];
     }
-    else{
-        resume();
+
+    if (soundOn) {
+      const StopSounds = new Audio();
+      StopSounds.src = "./media/audio/" + randomStopSounds();
+      StopSounds.play();
     }
-  });
+  } else {
+    resume();
+  }
+});
 
 // muting shia
 
 const muteBtn = document.getElementById("mute-btn");
 let soundOn = true;
 
-muteBtn.addEventListener("click", muteSound)
+muteBtn.addEventListener("click", muteSound);
 
-function muteSound(){
-    soundOn ? soundOn = false : soundOn = true;
-    soundOn ? muteBtn.src = "./media/images/sound-on-icon.png" : muteBtn.src = "./media/images/mute-icon.png";
-};
+function muteSound() {
+  soundOn ? (soundOn = false) : (soundOn = true);
+  soundOn
+    ? (muteBtn.src = "./media/images/sound-on-icon.png")
+    : (muteBtn.src = "./media/images/mute-icon.png");
+}
 
 // changing background
 
 const backgroundBtn = document.getElementById("bg-btn");
-backgroundBtn.addEventListener("click", changeBackground)
+backgroundBtn.addEventListener("click", changeBackground);
 
-let allBackgrounds = ["url('./media/images/bgs/ed.jpg')", "url('./media/images/bgs/haka.jpg')", "url('./media/images/bgs/cat.jpg')", "url('./media/images/bgs/mosh.jpg')", "url('./media/images/bgs/bodybuilders.jpg')", "url('./media/images/bgs/meerkats.jpg')", "url('./media/images/bgs/party.jpg')", "url('./media/images/bgs/wedding.jpg')", "url('./media/images/bgs/photogenic.jpg')", "url('./media/images/bgs/space.jpg')", "url('./media/images/bgs/bathroom.jpg')"]
+let allBackgrounds = [
+  "url('./media/images/bgs/ed.jpg')",
+  "url('./media/images/bgs/haka.jpg')",
+  "url('./media/images/bgs/cat.jpg')",
+  "url('./media/images/bgs/mosh.jpg')",
+  "url('./media/images/bgs/bodybuilders.jpg')",
+  "url('./media/images/bgs/meerkats.jpg')",
+  "url('./media/images/bgs/party.jpg')",
+  "url('./media/images/bgs/wedding.jpg')",
+  "url('./media/images/bgs/photogenic.jpg')",
+  "url('./media/images/bgs/space.jpg')",
+  "url('./media/images/bgs/bathroom.jpg')",
+];
 
 function changeBackground() {
-    
-    document.body.style.backgroundImage = allBackgrounds.pop();
-    allBackgrounds.splice(0, 0, document.body.style.backgroundImage)
+  document.body.style.backgroundImage = allBackgrounds.pop();
+  allBackgrounds.splice(0, 0, document.body.style.backgroundImage);
 
-    /* BY ORDER:
+  /* BY ORDER:
 
         document.body.style.backgroundColor = allBackgrounds.pop()
         allBackgrounds.splice(0, 0, document.body.style.backgroundColor) */
 
-    /* RANDOMIC:
+  /* RANDOMIC:
 
     let randomIndex = Math.floor(Math.random() * allBackgrounds.length);        
     let randomBackground = allBackgrounds[randomIndex];
@@ -165,37 +193,37 @@ function changeBackground() {
         randomBackground = allBackgrounds[randomIndex]
     }
     return document.body.style.backgroundColor = randomBackground; */
-};
+}
 
-// chancing background for mobile
+// changing background for mobile
 
 const backgroundBtnMob = document.getElementById("bg-btn-mob");
-backgroundBtnMob.addEventListener("click", changeBackgroundMob)
+backgroundBtnMob.addEventListener("click", changeBackgroundMob);
 
-let allBackgroundsMob = ["url('./media/images/bgs-mob/ed.jpg')", "url('./media/images/bgs-mob/cat.jpg')", "url('./media/images/bgs-mob/eiffel.jpg')", "url('./media/images/bgs-mob/meerkats.jpg')", "url('./media/images/bgs-mob/space.jpg')"]
+let allBackgroundsMob = [
+  "url('./media/images/bgs-mob/ed.jpg')",
+  "url('./media/images/bgs-mob/cat.jpg')",
+  "url('./media/images/bgs-mob/eiffel.jpg')",
+  "url('./media/images/bgs-mob/meerkats.jpg')",
+  "url('./media/images/bgs-mob/space.jpg')",
+];
 
 function changeBackgroundMob() {
-    document.body.style.backgroundImage = allBackgroundsMob.pop();
-    allBackgroundsMob.splice(0, 0, document.body.style.backgroundImage)
-};
+  document.body.style.backgroundImage = allBackgroundsMob.pop();
+  allBackgroundsMob.splice(0, 0, document.body.style.backgroundImage);
+}
 
 // follow the cursor
 
 let eyesElement = document.getElementsByClassName("eyeballs");
 
 document.onmousemove = function () {
-    let x = event.clientX * 100 / window.innerWidth + "%";
-    let y = event.clientY * 100 / window.innerHeight + "%";
+  let x = (event.clientX * 100) / window.innerWidth + "%";
+  let y = (event.clientY * 100) / window.innerHeight + "%";
 
-    for(i = 0; i < 2; i++){
- 
-        eyesElement[i].style.left = x;
-        eyesElement[i].style.top = y;
-        eyesElement[i].style.transform = "translate(-"+x+",-"+y+")";
-    }
+  for (i = 0; i < 2; i++) {
+    eyesElement[i].style.left = x;
+    eyesElement[i].style.top = y;
+    eyesElement[i].style.transform = `translate(-${x}, -${y})`;
+  }
 };
-
-
-
-
-
